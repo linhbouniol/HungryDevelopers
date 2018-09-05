d3490456fb81050990a6fe4f5c96768f4624bb42
Linh Bouniol <paulalinh89@gmail.com>
2018-09-05 16:28:43 -0700

    Finished project

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -4,7 +4,14 @@ import Cocoa
 
 class Spoon {
     
-    var spoonIsPickedUp: Bool = false
+//    var spoonIsPickedUp: Bool = false
+
+    var index: Int
+    
+    init(index: Int) { // initialize because you dont want to give it a default value right away
+        self.index = index
+    }
+    
     private let lock = NSLock()
     
     func pickUp() {
@@ -40,9 +47,24 @@ class Developer {
     func think() {
         // Think until leftSpoon is available, when it is, pick it up       -> spoon available means spoon isn't picked up?
         // Think until rightSpoon is available, when it is, pick it up
-        print("\(name) is picking up spoons")
-        leftSpoon.pickUp()
-        rightSpoon.pickUp()
+        
+//        print("\(name) is picking up spoons")
+//        leftSpoon.pickUp()
+//        print("\(name) picked up left spoon")
+//        rightSpoon.pickUp()
+//        print("\(name) picked up right spoon")
+        
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+//            print("\(name) picked up left spoon")
+            rightSpoon.pickUp()
+//            print("\(name) picked up right spoon")
+        } else {
+            rightSpoon.pickUp()
+//            print("\(name) picked up right spoon")
+            leftSpoon.pickUp()
+//            print("\(name) picked up left spoon")
+        }
     }
     
     func eat() {
@@ -51,7 +73,7 @@ class Developer {
          We want to wait between 0 and 10 seconds: 10 * drand48 * 1_000_000
         */
         print("\(name) is going to eat")
-        let randomSecond = UInt32(10 * drand48() * 1_000_000)
+        let randomSecond = UInt32(3 * drand48() * 1_000_000)
         
         // Wait some seconds then put down rightSpoon, then leftSpoon
         usleep(randomSecond)
@@ -71,11 +93,11 @@ class Developer {
 
 // Create 5 spoons and 5 developers
 
-var spoon1 = Spoon()
-var spoon2 = Spoon()
-var spoon3 = Spoon()
-var spoon4 = Spoon()
-var spoon5 = Spoon()
+var spoon1 = Spoon(index: 1)
+var spoon2 = Spoon(index: 2)
+var spoon3 = Spoon(index: 3)
+var spoon4 = Spoon(index: 4)
+var spoon5 = Spoon(index: 5)
 
 var developersArray = [Developer]()
 
